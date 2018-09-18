f510b5c8f87fd4ca7708ed039bea858d550cc776
Valentyn Khoroshchak <smartgtsua@gmail.com>
2018-09-18 23:21:56 +0300

    Fixed issue#2

--- CTriangle.cs
+++ CTriangle.cs
@@ -15,90 +15,73 @@
         Purple
     }
 
-    public class CTriangle
+    public interface IColor
     {
-        
-        public interface IColor
+        Colors Color { get; }
+    }
+
+    public struct ColoredSide : IColor
+    {
+        private Colors color;
+        private double length;
+
+        public ColoredSide(Colors color, double length)
         {
-            Colors Color { get; }
+            this.color = color;
+            this.length = length;
         }
 
-        public struct ColoredSide : IColor
+        public Colors Color
         {
-            private Colors color;
-            private double length;
-
-            public ColoredSide(Colors color, double length)
-            {
-                this.color = color;
-                this.length = length;
-            }
-
-            public Colors Color
-            {
-                get { return this.color; }
-            }
-
-            public double Length
-            {
-                get
-                {
-                    return this.length;
-                }
-            }
+            get { return this.color; }
         }
 
-        public class ColoredTriangle
+        public double Length
         {
-            private ColoredSide firstSide;
-            private ColoredSide secondSide;
-            private ColoredSide thirdSide;
-
-            public ColoredTriangle()
+            get
             {
-            }
-
-            public ColoredTriangle(Colors firstSideColor, double firstSideLength, Colors secondSideColor, double secondSideLength, Colors thirdSideColor, double thirdSideLength)
-            {
-                this.firstSide = new ColoredSide(firstSideColor, firstSideLength);
-                this.secondSide = new ColoredSide(secondSideColor, secondSideLength);
-                this.thirdSide = new ColoredSide(thirdSideColor, thirdSideLength);
-            }
-
-            public void Input(string line)
-            {
-                var textLine = line.Split(' ');
-                this.firstSide = new ColoredSide((Colors)Enum.Parse(typeof(Colors), textLine[0]), double.Parse(textLine[1]));
-                this.secondSide = new ColoredSide((Colors)Enum.Parse(typeof(Colors), textLine[2]), double.Parse(textLine[3]));
-                this.thirdSide = new ColoredSide((Colors)Enum.Parse(typeof(Colors), textLine[4]), double.Parse(textLine[5]));
-            }
-
-            public override string ToString()
-            {
-                return $"First side color: {firstSide.Color}, with length: {firstSide.Length}," +
-                    $" Second side color: {secondSide.Color}, with length: {secondSide.Length}," +
-                    $" Third side color: {thirdSide.Color}, with length: {thirdSide.Length}.";
-            }
-
-            public void Print(string path)
-            {
-                StreamWriter sw = new StreamWriter(path, true);
-                sw.WriteLine(this.ToString());
-                sw.Close();
+                return this.length;
             }
         }
+    }
 
-       public static void Main(string[] args)
+    public class ColoredTriangle
+    {
+        private ColoredSide firstSide;
+        private ColoredSide secondSide;
+        private ColoredSide thirdSide;
+
+        public ColoredTriangle()
         {
-            //var strs = File.ReadAllLines("input.txt");
-            //List<ColoredTriangle> ctList = new List<ColoredTriangle>();
-            //foreach (var item in strs)
-            //{
-            //    ColoredTriangle ct = new ColoredTriangle();
-            //    ct.Input(item);
-            //    ctList.Add(ct);
-            //    ct.Print("output.txt");
-            //}
+        }
+
+        public ColoredTriangle(Colors firstSideColor, double firstSideLength, Colors secondSideColor, double secondSideLength, Colors thirdSideColor, double thirdSideLength)
+        {
+            this.firstSide = new ColoredSide(firstSideColor, firstSideLength);
+            this.secondSide = new ColoredSide(secondSideColor, secondSideLength);
+            this.thirdSide = new ColoredSide(thirdSideColor, thirdSideLength);
+        }
+
+        public void Input(string line)
+        {
+            var textLine = line.Split(' ');
+            this.firstSide = new ColoredSide((Colors)Enum.Parse(typeof(Colors), textLine[0]), double.Parse(textLine[1]));
+            this.secondSide = new ColoredSide((Colors)Enum.Parse(typeof(Colors), textLine[2]), double.Parse(textLine[3]));
+            this.thirdSide = new ColoredSide((Colors)Enum.Parse(typeof(Colors), textLine[4]), double.Parse(textLine[5]));
+        }
+
+        public override string ToString()
+        {
+            return $"First side color: {firstSide.Color}, with length: {firstSide.Length}," +
+                $" Second side color: {secondSide.Color}, with length: {secondSide.Length}," +
+                $" Third side color: {thirdSide.Color}, with length: {thirdSide.Length}.";
+        }
+
+        public void Print(string path)
+        {
+            StreamWriter sw = new StreamWriter(path, true);
+            sw.WriteLine(this.ToString());
+            sw.Close();
         }
     }
 }
