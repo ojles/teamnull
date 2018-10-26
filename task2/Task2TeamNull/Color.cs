9e025ff6956fbf7d5c345fed99543ee8c9dffc74
Valentyn Khoroshchak <smartgtsua@gmail.com>
2018-10-27 00:42:47 +0300

    Initialized project, created basic classes, xml doc, etc.

---
+++ task2/Task2TeamNull/Color.cs
@@ -0,0 +1,44 @@
+﻿using System.Xml.Serialization;
+
+namespace Task2TeamNull
+{
+    public class Color
+    {
+        /// <summary>
+        /// Gets/Sets an amount of red colour in RGB specification
+        /// </summary>
+        [XmlAttribute]
+        public byte R { get; set; }
+
+        /// <summary>
+        /// Gets/Sets an amount of green colour in RGB specification
+        /// </summary>
+        [XmlAttribute]
+        public byte G { get; set; }
+
+        /// <summary>
+        /// Gets/Sets an amount of blue colour in RGB specification
+        /// </summary>
+        [XmlAttribute]
+        public byte B { get; set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Color"/> class. 
+        /// </summary>
+        public Color() { }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Color"/> class
+        /// Function to set colour of the border using RGB specification
+        /// </summary>
+        /// <param name="r">Amount of red</param>
+        /// <param name="g">Amount of green</param>
+        /// <param name="b">Amount of blue</param>
+        public Color(byte r, byte g, byte b)
+        {
+            R = r;
+            G = g;
+            B = b;
+        }
+    }
+}
---
+++ task2/Task2TeamNull/point.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Xml.Serialization;
+
+namespace Task2TeamNull
+{
+    /// <summary>
+    /// Class to represent a point in two-dimensional space
+    /// </summary>
+
+    [Serializable]
+    public class Point
+
+    {
+        /// <summary>
+        /// Gets/Sets X coordinate of a point
+        /// </summary>
+        [XmlAttribute]
+        public double X { get; set; }
+
+        /// <summary>
+        /// Gets/Sets Y coordinate of a point
+        /// </summary>
+        [XmlAttribute]
+        public double Y { get; set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Point"/> class 
+        /// </summary>
+        public Point()
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Point"/> class  
+        /// </summary>
+        /// <param name="x">X coordinate of a point</param>
+        /// <param name="y">Y coordinate of a point</param>
+        public Point(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+}
