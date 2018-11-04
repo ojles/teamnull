4d979991bbb9ec0e9c9cc8f26ffc6d83b1e6d443
samozvon <densamozvon@gmail.com>
2018-11-04 22:04:20 +0200

    Add service for Pentagon serialization.

---
+++ task2/Pentagon/Domain/Canvas.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pentagon.Domain
+{
+    /// <summary>
+    /// Class to represent a Canvas with pentagons
+    /// </summary>
+    public class Canvas
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Canvas"/>
+        /// </summary>
+        public Canvas() { pentagonList = new List<task2.Pentagon>(); }
+
+        /// <summary>
+        /// List of the <see cref="task2.Pentagon"/>
+        /// </summary>
+        public List<task2.Pentagon> pentagonList { get; set; }
+    }
+}
