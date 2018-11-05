932eb67f12b4be502661c0b8793163b3e1566536
Oles Pankiv <ojlesua@gmail.com>
2018-11-05 23:48:23 +0200

    Move CanvasService to Service/ dir

---
+++ task2/Pentagon/Service/CanvasService.cs
@@ -0,0 +1,39 @@
+﻿using System.Xml.Serialization;
+using System.IO;
+using Task2.Domain;
+
+namespace Task2.Service
+{
+    /// <summary>
+    /// Class to represent a serializer for an object of the Canvas
+    /// </summary>
+    public class CanvasService
+    {
+        /// <summary>
+        /// Serializes an instance of the <see cref="Canvas"/>
+        /// </summary>
+        /// <param name="canvas">Object of the Canvas</param>
+        /// <param name="fileName">Name of the file</param>
+        public void Save(Canvas canvas, string fileName)
+        {
+            using (var stream = File.Create(fileName))
+            {
+                var serializer = new XmlSerializer(typeof(Canvas));
+                serializer.Serialize(stream, canvas);
+            }
+        }
+
+        /// <summary>
+        /// Deserializes an instance of the <see cref="Canvas"/>
+        /// </summary>
+        /// <param name="fileName">Name of the file</param>
+        public Canvas Get(string fileName)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(Canvas));
+            using (var reader = new StreamReader(fileName))
+            {
+                return (Canvas)serializer.Deserialize(reader);
+            }
+        }
+    }
+}
