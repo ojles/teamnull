6483c5b234785ed61e3dd4841ea3e04f837b5be3
Oles Pankiv <ojlesua@gmail.com>
2018-11-26 22:03:19 +0200

    Refactor Meal and MealGroup

--- task3/Task3/Domain/MealGroup.cs
+++ task3/Task3/Domain/MealGroup.cs
@@ -4,26 +4,25 @@ namespace Task3
 {
     class MealGroup
     {
-        public string Name
-        {
-            get { return Name; }
-            set
-            {
-                if (value == null)
-                {
-                    throw new System.ArgumentNullException("name", "Parameter cannot be null");
-                }
-
-                Name = value;
-            }
-        }
-
+        public string Name { get; set; }
         public List<Meal> Meals { get; set; } = new List<Meal>();
 
-        public MealGroup(string name, List<Meal> meals)
+        private MealGroup()
         {
-            Name = name;
-            Meals = meals;
+        }
+
+        public static MealGroup Create(string name, List<Meal> meals)
+        {
+            if (name == null)
+            {
+                throw new System.ArgumentNullException("name", "Parameter cannot be null");
+            }
+
+            return new MealGroup
+            {
+                Name = name,
+                Meals = meals
+            };
         }
     }
 }
