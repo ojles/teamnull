ffc4f293c9da080fe2296e10860b54a64e873bd5
Valentyn Khoroshchak <smartgtsua@gmail.com>
2018-11-27 15:03:42 +0200

    Add hamburger menu

--- task3/Task3/MainWindow.xaml.cs
+++ task3/Task3/MainWindow.xaml.cs
@@ -1,71 +1,27 @@
 ﻿using System.Collections.Generic;
 using System.Windows;
+using Task3.Pages;
 
 namespace Task3
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-
-    class Item
-    {
-        public string Name
-        {
-            get;
-            set;
-        }
-        public int Qty
-        {
-            get;
-            set;
-        }
-        public double Price
-        {
-            get;
-            set;
-        }
-        public double Sum
-        {
-            get
-            {
-               return this.Qty * this.Price;
-            }
-            set
-            {
-                Sum = value;
-            }
-        }
-        public string ImgPath
-        {
-            get;
-            set;
-        }
-        public override string ToString()
-        {
-            return string.Format("{0} {1} {2} {3}", Name, Qty, Price, Sum);
-        }
-    }
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
-            Item i1 = new Item();
-            i1.Name = "Coffee";
-            i1.Price = 20;
-            i1.Qty = 3;
-            i1.ImgPath = "/Images/cafe.png";
-            Item i2 = new Item();
-            i2.Name = "Tea";
-            i2.Price = 10;
-            i2.Qty = 3;
-            i2.ImgPath = "/Images/tea.png";
-            List<Item> items = new List<Item>();
-            items.Add(i1);
-            items.Add(i2);
-            //lvUsers.ItemsSource = items;
-            Items.ItemsSource= items;
-            
+        }
+
+        private void MenuPageClick(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            MainFrame.Navigate(new MenuPage());
+        }
+
+        private void ReceiptPageClick(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            MainFrame.Navigate(new ReceiptPage());
         }
     }
 }
