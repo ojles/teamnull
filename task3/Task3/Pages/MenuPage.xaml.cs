f56497fde8bd6f9d0dad546ab347d1f37bc02d52
Oles Pankiv <ojlesua@gmail.com>
2018-12-03 00:13:55 +0200

    Fix SushiMenu resource

--- task3/Task3/Pages/MenuPage.xaml.cs
+++ task3/Task3/Pages/MenuPage.xaml.cs
@@ -13,25 +13,8 @@ namespace Task3.Pages
         {
             InitializeComponent();
 
-            MealGroup mg1 = (MealGroup)Application.Current.FindResource("mg1");
-
-            MealGroup group = MealGroup.Create("First Dish");
-            group.AddMeal(Meal.Create("first name", 123));
-            group.AddMeal(Meal.Create("second name", 23));
-            group.AddMeal(Meal.Create("third name", 12));
-
-            MealGroup group2 = MealGroup.Create("Second Dish");
-            group2.AddMeal(Meal.Create("fourth name", 710));
-            group2.AddMeal(Meal.Create("fifth name", 576));
-            group2.AddMeal(Meal.Create("sixth name", 321));
-
-            List<MealGroup> menu = new List<MealGroup>
-            {
-                group,
-                group2,
-                mg1
-            };
-            Menu.ItemsSource = menu;
+            Menu menu = (Menu)Application.Current.FindResource("SushiMenu");
+            Menu.ItemsSource = menu.MealGroups;
         }
     }
 }
