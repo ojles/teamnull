67b90e523b4e6ebaa4fe245e6e29e27c116ba7ad
Oles Pankiv <ojlesua@gmail.com>
2018-12-18 14:32:40 +0200

    Initialized project, created methods Connect() and Disconnect()

---
+++ Task4/Task4/DataBase.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace Task4
+{
+   public class DataBase
+    {
+        private readonly string connectionString;
+        private SqlConnection connection;
+
+        public DataBase(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
+        public bool Connect()
+        {            
+            connection = new SqlConnection(connectionString);
+            connection.Open();
+            if (connection.State == ConnectionState.Open)
+            {
+                return true;
+            }
+                
+            return false;
+        }
+
+        public bool Disconnect()
+        {          
+            if (connection.State == ConnectionState.Open)
+            {
+                connection.Close();
+                return connection.State == ConnectionState.Closed;
+            }
+
+            return false;
+        }       
+   }
+}
---
+++ Task4/Task4/Task.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Task4
+{
+    class Task
+    {
+        private DataBase db;
+
+        public Task()
+        {
+            db = new DataBase(@"Data Source = (localdb)\MSSQLLocalDB; Initial Catalog = Northwind; Integrated Security = True");
+        }
+
+        private void ConnectToDatabase()
+        {
+            db.Connect();          
+            Console.WriteLine("\n Successfully connected to the database \n\n");
+        }
+
+        private void DisconnectFromDatabase()
+        {
+            db.Disconnect();          
+            Console.WriteLine("\n Successfully disconnected from the database \n\n");
+        }
+
+        public void ExecuteTask()
+        {           
+            ConnectToDatabase();                   
+            DisconnectFromDatabase();           
+        }
+    }
+}
