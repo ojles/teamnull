6ddfb04253b8e1905ba3bdfa43b0f04d6d7fc220
Gumbew <smartgtsua@gmail.com>
2018-12-19 16:14:35 +0200

    Add tests to task2

---
+++ task2/PentagonTest/CanvasServiceTest.cs
@@ -0,0 +1,51 @@
+﻿using Task2.Service;
+using Task2.Domain;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PentagonTest
+{
+    [TestClass]
+    public class CanvasServiceTest
+    {
+        public CanvasService canvasService = new CanvasService();
+
+        [TestMethod]
+        public void TestSaveCanvas()
+        {
+            Pentagon pentagon = new Pentagon();
+            pentagon.AddPoint(new Point(3, 2));
+            pentagon.AddPoint(new Point(3, 2));
+            pentagon.AddPoint(new Point(3, 2));
+            pentagon.AddPoint(new Point(3, 2));
+            pentagon.AddPoint(new Point(3, 2));
+
+            Canvas canvas = new Canvas();
+            canvas.AddPentagon(pentagon);
+
+            canvasService.Save(canvas, "test1");
+
+            Assert.IsTrue(File.Exists("test1"));
+        }
+
+        [TestMethod]
+        public void TestGetCanvas()
+        {
+            Pentagon pentagon = new Pentagon();
+            pentagon.AddPoint(new Point(3, 2));
+            pentagon.AddPoint(new Point(3, 2));
+            pentagon.AddPoint(new Point(3, 2));
+            pentagon.AddPoint(new Point(3, 2));
+            pentagon.AddPoint(new Point(3, 2));
+
+            Canvas canvas = new Canvas();
+            canvas.AddPentagon(pentagon);
+
+            canvasService.Save(canvas, "test2");
+
+            Canvas retrievedCanvas = canvasService.Get("test2");
+
+            Assert.AreEqual(retrievedCanvas.Pentagons.Count, canvas.Pentagons.Count);
+        }
+    }
+}
---
+++ task2/PentagonTest/PentagonTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Task2.Domain;
+
+namespace PentagonTest
+{
+    [TestClass]
+    public class PentagonTest
+    {
+        [TestMethod]
+        public void TestPentagonIsCompleted()
+        {
+            Pentagon pentagon = new Pentagon();
+            pentagon.AddPoint(new Point(1, 2));
+            pentagon.AddPoint(new Point(1, 2));
+            pentagon.AddPoint(new Point(1, 2));
+            Assert.IsFalse(pentagon.IsCompleted());
+            pentagon.AddPoint(new Point(1, 2));
+            pentagon.AddPoint(new Point(1, 2));
+            Assert.IsTrue(pentagon.IsCompleted());
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(IndexOutOfRangeException))]
+        public void TestPentagonPointOverflow()
+        {
+            Pentagon pentagon = new Pentagon();
+            pentagon.AddPoint(new Point(1, 2));
+            pentagon.AddPoint(new Point(1, 2));
+            pentagon.AddPoint(new Point(1, 2));
+            pentagon.AddPoint(new Point(1, 2));
+            pentagon.AddPoint(new Point(1, 2));
+            pentagon.AddPoint(new Point(1, 2));
+        }
+    }
+}
