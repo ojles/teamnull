e51b6233e2071c239cc2455e5e789e8f99c36a44
Oles Pankiv <ojlesua@gmail.com>
2018-12-24 00:05:27 +0200

    Finish queries 9, 10, 15, 20

--- task4/Task4/Query.cs
+++ task4/Task4/Query.cs
@@ -12,18 +12,27 @@ namespace Task4
             "query 6",
             "query 7",
             "query 8",
-            "SELECT `FirstName`, `LastName` FROM `Employees` WHERE `BirthDate` = (SELECT MIN(`BirthDate`) FROM `Employees`)",
+            "SELECT `FirstName`, `LastName` FROM `Employees` WHERE `BirthDate`=(SELECT MIN(`BirthDate`) FROM `Employees`)",
             "SELECT `FirstName`, `LastName` FROM `Employees` ORDER BY `BirthDate` LIMIT 3",
             "query 11",
             "query 12",
             "query 13",
             "query 14",
-            "query 15",
+            "SELECT `e`.`FirstName`, `e`.`LastName`, COUNT(`o`.`OrderID`) AS `Orders made` "
+                + "FROM `Employees` as `e` INNER JOIN `Orders` AS `o` "
+                + "ON `e`.`EmployeeID`=`o`.`EmployeeID` "
+                + "WHERE YEAR(`o`.`OrderDate`)=1997 "
+                + "GROUP BY `e`.`EmployeeID`",
             "query 16",
             "query 17",
             "query 18",
             "query 19",
-            "query 20"
+            "SELECT `c`.`ContactName`, `c`.`ContactTitle` "
+                + "FROM `Customers` AS `c` "
+                + "INNER JOIN `Orders` AS `o` ON `c`.`CustomerID`=`o`.`CustomerID` "
+                + "INNER JOIN `Order Details` AS `od` ON `o`.`OrderID`=`od`.`OrderID` "
+                + "INNER JOIN `Products` AS `p` ON `od`.`ProductID`=`p`.`ProductID` "
+                + "WHERE `p`.`ProductName`='Tofu';"
         };
     }
 }
