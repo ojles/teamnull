7ee26058884deb8f4f411549fc6e7911af825844
Oles Pankiv <ojlesua@gmail.com>
2018-12-24 23:33:24 +0200

    Add Database class

---
+++ task4/Task4/Database.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+
+namespace Task4
+{
+    public sealed class Database
+    {
+        private const string connectionString = "server=localhost;user id=root;database=northwind; password=smart";                          
+        private static readonly MySqlConnection Connection = new MySqlConnection(connectionString);
+
+        public static Connection GetConnection()
+        {
+            if (!Connection.IsOpen())
+            {
+                Connection.Open();
+            }
+
+            return Connection;
+        }
+    }
+}
