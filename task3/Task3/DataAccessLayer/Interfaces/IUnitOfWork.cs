b48d8a31db4a0d4d0d2a5233e48b0424a6af0850
Gumbew <smartgtsua@gmail.com>
2018-12-26 00:28:11 +0200

    Added interfaces IGenericRepository and IUnitOfWork

---
+++ task3/Task3/DataAccessLayer/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3.DataAccessLayer.Interfaces
+{
+    public class OrderContext : DbContext
+    {     
+        public DbSet<Order> Orders { get; set; }
+    }
+}
